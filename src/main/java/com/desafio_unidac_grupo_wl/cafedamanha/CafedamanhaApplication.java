893f3b97235b4a03270885cc9e9cbc5f2e813c65
MayconL27 <mayconleandro2008@gmail.com>
2022-07-03 15:36:07 -0300

    first commit

---
+++ src/main/java/com/desafio_unidac_grupo_wl/cafedamanha/CafedamanhaApplication.java
@@ -0,0 +1,13 @@
+package com.desafio_unidac_grupo_wl.cafedamanha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafedamanhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafedamanhaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio_unidac_grupo_wl/cafedamanha/CafedamanhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio_unidac_grupo_wl.cafedamanha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafedamanhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
