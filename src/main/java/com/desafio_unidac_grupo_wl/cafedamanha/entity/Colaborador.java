3c93b6ffb1329d1227ebbbc5d2596e10aaa86113
MayconL27 <mayconleandro2008@gmail.com>
2022-07-03 15:42:46 -0300

    Entidade Colaborador

---
+++ src/main/java/com/desafio_unidac_grupo_wl/cafedamanha/entity/Colaborador.java
@@ -0,0 +1,37 @@
+package com.desafio_unidac_grupo_wl.cafedamanha.entity;
+
+public class Colaborador {
+
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String cafe;
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public String getCafe() {
+        return cafe;
+    }
+    public void setCafe(String cafe) {
+        this.cafe = cafe;
+    }
+
+    
+    
+}
