c655911d915d41e11ea252ed6098c25da5464ee7
MayconL27 <mayconleandro2008@gmail.com>
2022-07-03 15:49:55 -0300

    criando atributos de Colaborador

--- src/main/java/com/desafio_unidac_grupo_wl/cafedamanha/entity/Colaborador.java
+++ src/main/java/com/desafio_unidac_grupo_wl/cafedamanha/entity/Colaborador.java
@@ -1,12 +1,24 @@
 package com.desafio_unidac_grupo_wl.cafedamanha.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Colaborador {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //auto incremente.
     private Long id;
+    @Column(nullable = false) // Not null.
     private String nome;
+    @Column(nullable = false)
     private String cpf;
+    @Column(nullable = false)
     private String cafe;
-    
+
     public Long getId() {
         return id;
     }
