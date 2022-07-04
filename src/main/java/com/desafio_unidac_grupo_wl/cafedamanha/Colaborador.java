e0fab052b9e24ddb191d113cfd6f5241981b4e28
MayconL27 <mayconleandro2008@gmail.com>
2022-07-04 15:21:03 -0300

    Conexão com o banco testado e funcionado

--- src/main/java/com/desafio_unidac_grupo_wl/cafedamanha/Colaborador.java
+++
@@ -1,5 +0,0 @@
-package com.desafio_unidac_grupo_wl.cafedamanha;
-
-public class Colaborador {
-
-}
--- src/main/java/com/desafio_unidac_grupo_wl/cafedamanha/ColaboradorREST.java
+++ src/main/java/com/desafio_unidac_grupo_wl/cafedamanha/ColaboradorREST.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,10 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.desafio_unidac_grupo_wl.cafedamanha.database.RepositoryColaborador;
 import com.desafio_unidac_grupo_wl.cafedamanha.entity.Colaborador;
 
-
-
 @RestController
-@RequestMapping("/colaborador")
+@RequestMapping("/colaboradores")
 public class ColaboradorREST {
     @Autowired
     private RepositoryColaborador repository;
@@ -44,10 +43,8 @@ public class ColaboradorREST {
     }
 
     // Excluir
-    @DeleteMapping
-    public void excluir(@RequestBody Colaborador colaborador) {
-        repository.delete(colaborador);
-    }
-    
-    
+    @DeleteMapping("/{id}")
+    public void excluir(@PathVariable Long id) {
+        repository.deleteById(id);
+    }   
 }
